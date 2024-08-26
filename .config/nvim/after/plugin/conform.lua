b21f59ef70eec9ad342c38cff712e4317b120688
Reed <reedjmerrill@gmail.com>
2024-08-25 20:35:54 -0700

    bug fix

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -1,7 +1,6 @@
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
 		python = { "black" },
 		r = { "styler" },
 		javascript = { "prettier" },
@@ -10,10 +9,10 @@ require("conform").setup({
 	},
 })
 
--- format on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
+require("conform").setup({
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 })
