d643dea08d159f5a14de8a461dc47292a19be5f1
Reed <reedjmerrill@gmail.com>
2024-08-30 19:20:17 -0700

    remove oil and install yazi

--- .config/nvim/after/plugin/oil.lua
+++
@@ -1,2 +0,0 @@
--- enter oil
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Enter Oil" })
---
+++ .config/nvim/after/plugin/yazi.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "-", "<cmd>Yazi<cr>", { noremap = true, desc = "Launch yazi" })
+vim.keymap.set("n", "<leader>cw", "<cmd>Yazi cwd<cr>", { noremap = true, desc = "Launch yazi in cwd" })
+vim.keymap.set("n", "-", "<cmd>Yazi toggle<cr>", { noremap = true, desc = "toggle yazi" })
