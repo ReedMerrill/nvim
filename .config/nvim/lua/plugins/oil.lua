da856e4aab6e5313d47ef03e94d9036a1fac891a
Reed Merrill <reed@reeds-mbp.ucalgary.ca>
2024-09-09 11:48:51 -0600

    config oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -54,7 +54,7 @@ return {
 				-- Set to `false` to disable, or "name" to keep it on the file names
 				constrain_cursor = "editable",
 				-- Set to true to watch the filesystem for changes and reload oil
-				watch_for_changes = false,
+				watch_for_changes = true,
 				-- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
 				-- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
 				-- Additionally, if it is a string that matches "actions.<name>",
@@ -91,7 +91,7 @@ return {
 				use_default_keymaps = true,
 				view_options = {
 					-- Show files and directories that start with "."
-					show_hidden = false,
+					show_hidden = true,
 					-- This function defines what is considered a "hidden" file
 					is_hidden_file = function(name, bufnr)
 						return vim.startswith(name, ".")
@@ -104,7 +104,7 @@ return {
 					-- so you may want to set to false if you work with large directories.
 					natural_order = true,
 					-- Sort file and directory names case insensitive
-					case_insensitive = false,
+					case_insensitive = true,
 					sort = {
 						-- sort order can be "asc" or "desc"
 						-- see :help oil-columns to see which columns are sortable
