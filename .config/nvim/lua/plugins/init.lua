ee2fabe8e21056fa12244454c6c63ca194c7a996
Reed Merrill <reed@Reeds-MacBook-Pro.local>
2024-09-09 08:22:11 -0600

    quick changes

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,7 +3,7 @@ return {
 		"MeanderingProgrammer/render-markdown.nvim",
 		opts = {
 			bullet = {
-				icons = { "• " },
+				icons = { "•" },
 			},
 		},
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
