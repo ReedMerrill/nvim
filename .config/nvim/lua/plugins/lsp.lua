0c56bdf2ccb4f57e68722a85e9626936ecc7ae6f
Reed Merrill <reed@Reeds-MacBook-Pro.local>
2024-10-29 11:46:57 -0600

    remove r-language-server

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -140,7 +140,6 @@ return {
 				html = {},
 				ts_ls = {},
 				bashls = {},
-				r_language_server = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
