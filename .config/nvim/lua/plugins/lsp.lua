130062deb70c281d8a1432952722fb301ce0cf59
Reed <reedjmerrill@gmail.com>
2025-07-24 08:23:35 -0600

    VS Code Neovim compatibility

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -54,17 +54,19 @@ return {
 					--    See `:help CursorHold` for information about when this is executed
 					-- When the cursor moves, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.server_capabilities.documentHighlightProvider then
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							callback = vim.lsp.buf.document_highlight,
-						})
+					-- functionality from the "VSCode Neovim" plugin... Somehow...
+          local vs_code = vim.g.vscode
+						if (not vs_code) and client and client.server_capabilities.documentHighlightProvider then
+							vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+								buffer = event.buf,
+								callback = vim.lsp.buf.document_highlight,
+							})
 
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							callback = vim.lsp.buf.clear_references,
-						})
-					end
+							vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+								buffer = event.buf,
+								callback = vim.lsp.buf.clear_references,
+							})
+						end
 				end,
 			})
 
