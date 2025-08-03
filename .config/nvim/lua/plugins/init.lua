ff20e7203d15e14d94ba5ce3e4e8f2d216dfeaa5
Reed <reedjmerrill@gmail.com>
2025-08-02 19:25:09 -0600

    add OSV (debug code running in a neovim instance)

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -147,7 +147,27 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	-- Debugger
-	{ "mfussenegger/nvim-dap" },
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"jbyuki/one-small-step-for-vimkind",
+		},
+		lazy = false,
+		config = function()
+			local dap = require("dap")
+			dap.configurations.lua = {
+				{
+					type = "nlua",
+					request = "attach",
+					name = "Attach to running Neovim instance",
+				},
+			}
+
+			dap.adapters.nlua = function(callback, config)
+				callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+			end
+		end,
+	},
 	-- DAP UI
 	{
 		"rcarriga/nvim-dap-ui",
