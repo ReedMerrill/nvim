ec7aad447069beee8eff02761301bda26f0b9dab
Reed <reedjmerrill@gmail.com>
2025-08-21 15:34:53 -0600

    add python to DAP plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -144,6 +144,8 @@ return {
 		lazy = false,
 		config = function()
 			local dap = require("dap")
+			local dap_python = require("dap-python")
+			dap_python.setup("python3")
 			dap.configurations.lua = {
 				{
 					type = "nlua",
