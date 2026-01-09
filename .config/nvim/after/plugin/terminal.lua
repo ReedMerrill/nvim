3d610d715b20d1a4e911216815310c424cce0636
Reed Merrill <reedjmerrill@gmail.com>
2026-01-08 19:21:28 -0700

    fix toggleterm keymaps

--- .config/nvim/after/plugin/terminal.lua
+++ .config/nvim/after/plugin/terminal.lua
@@ -1,2 +1,3 @@
 -- set snacks.terminal toggle keymap here, because it doesn't override the default tag stack command when set in the snacks init
-vim.keymap.set("n", "<leader>T", ":ToggleTerm direction=vertical<CR>", { desc = "Toggle Terminal" })
+vim.keymap.set("n", "<leader>T", ":ToggleTerm direction=horizontal<CR>", { desc = "Toggle Terminal (Horiz.)" })
+vim.keymap.set("n", "<leader>Tv", ":ToggleTerm direction=vertical<CR>", { desc = "Toggle Terminal (Vert.)" })
