03b1286a00d8c93c364e1fb5de6e217e62a705e9
Reed Merrill <reedjmerrill@gmail.com>
2026-02-04 09:27:27 -0700

    remove ultimate-autopairs

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -362,10 +362,4 @@ return {
 		},
 		cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
 	},
-	-- {
-	-- 	"altermo/ultimate-autopair.nvim",
-	-- 	event = { "InsertEnter", "CmdlineEnter" },
-	-- 	branch = "v0.6", --recommended as each new version will have breaking changes
-	-- 	opts = {},
-	-- },
 }
