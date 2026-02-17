44cac97e8259f11d8b8937d0e3f7247cd7a37de1
Reed <reedjmerrill@gmail.com>
2026-02-17 13:49:45 -0700

    copilot completions and filetype filtering

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -24,12 +24,12 @@ return {
 				},
 				suggestion = {
 					enabled = true,
-					auto_trigger = false,
-					hide_during_completion = true,
-					debounce = 75,
+					auto_trigger = true,
+					hide_during_completion = false,
+					debounce = 15,
 					trigger_on_accept = true,
 					keymap = {
-						accept = "<M-l>",
+						accept = "<C-y>",
 						accept_word = false,
 						accept_line = false,
 						next = "<M-]>",
@@ -38,15 +38,14 @@ return {
 					},
 				},
 				filetypes = {
+					-- Markdown is disabled by default
 					yaml = false,
-					markdown = false,
 					help = false,
 					gitcommit = false,
 					gitrebase = false,
 					hgcommit = false,
 					svn = false,
-					cvs = false,
-					["."] = false,
+					csv = false,
 				},
 				auth_provider_url = nil, -- URL to authentication provider, if not "https://github.com/"
 
